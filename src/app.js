84149d8bc2bf238f0d21a98ee46c13d7880cd632
Erik Stielstra <erik@limoengroen.nl>
2019-05-22 18:14:26 +0200

    Use separate file for routing.

--- src/app.js
+++ src/app.js
@@ -4,67 +4,13 @@
  */
 
 const express = require('express');
-const { param, validationResult } = require('express-validator/check');
-const { initStorage, addVote, getVoteAverage, getVoteCount } = require('./lib/votes-persistent');
+const { initStorage } = require('./lib/votes-persistent');
+const { getAverage, getStatistics, postVoteValidation, postVote } = require('./lib/routing');
 
 const app = express();
 
-/**
- * Callback: Return the vote average.
- */
-async function getAverage(req, res) {
-  const { id } = req.params;
-
-  res.json(await getVoteAverage(id));
-}
-
-/**
- * Callback: Return the vote statistics.
- */
-async function getStatistics(req, res) {
-  const { id } = req.params;
-
-  res.json({
-    average: await getVoteAverage(id),
-    count: await getVoteCount(id),
-  });
-}
-
-/**
- * Validation chain for postVote.
- *
- * @type {ValidationChain[]}
- */
-const postVoteValidation = [
-  param('value').isIn(['1', '2', '3', '4', '5']).withMessage('Parameter must be a value of 1 .. 5'),
-];
-
-/**
- * Callback: Store vote data.
- */
-async function postVote(req, res) {
-  const { id } = req.params;
-  const { value } = req.params;
-  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-
-  try {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      res.status(406).json({
-        error: errors.mapped(),
-      });
-    }
-
-    await addVote(id, value, ip);
-    res.json(await getVoteAverage(id));
-  } catch (err) {
-    // TODO Additional error handling.
-  }
-}
-
 initStorage();
 
-// Middle ware.
 app.get('/vote/:id', getAverage);
 app.get('/vote/:id/stats', getStatistics);
 app.post('/vote/:id/:value', postVoteValidation, postVote);
---
+++ src/lib/routing.js
@@ -0,0 +1,55 @@
+const { param, validationResult } = require('express-validator/check');
+const { addVote, getVoteAverage, getVoteCount } = require('./votes-persistent');
+
+/**
+ * Callback: Return the vote average.
+ */
+export async function getAverage(req, res) {
+  const { id } = req.params;
+
+  res.json(await getVoteAverage(id));
+}
+
+/**
+ * Callback: Return the vote statistics.
+ */
+export async function getStatistics(req, res) {
+  const { id } = req.params;
+
+  res.json({
+    average: await getVoteAverage(id),
+    count: await getVoteCount(id),
+  });
+}
+
+/**
+ * Validation chain for postVote.
+ *
+ * @type {ValidationChain[]}
+ */
+export const postVoteValidation = [
+  param('value').isIn(['1', '2', '3', '4', '5']).withMessage('Parameter must be a value of 1 .. 5'),
+];
+
+/**
+ * Callback: Store vote data.
+ */
+export async function postVote(req, res) {
+  const { id } = req.params;
+  const { value } = req.params;
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(406).json({
+        error: errors.mapped(),
+      });
+    }
+
+    await addVote(id, value, ip);
+    res.json(await getVoteAverage(id));
+  } catch (err) {
+    // TODO Additional error handling.
+  }
+}
