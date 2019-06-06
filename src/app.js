232d1c43e2cb073c33cdd5704883146e2e2d14d1
Erik Stielstra <erik@limoengroen.nl>
2019-06-06 11:50:12 +0200

    Handle uncaught exceptions.

--- src/app.js
+++ src/app.js
@@ -5,6 +5,14 @@ import { routerVote, handleError } from './router/vote';
 
 const app = express();
 
+// Register to log any uncaught exception. Exception terminates the application, they are likely due
+// to programmer errors (and let's be strict).
+process.on('uncaughtException', (err) => {
+  console.error(`${(new Date()).toISOString()} uncaughtException:`, err.message);
+  console.error(err.stack);
+  process.exit(1);
+});
+
 // Initialize persistent storage.
 initStorage();
 
